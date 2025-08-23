e05eeec452e01bf25a8724b06dfe20659e4276d2
Patrick Surry <patrick.surry@gmail.com>
2025-08-22 21:25:07 -0400

    add test

---
+++ tests/test_piece.py
@@ -0,0 +1,19 @@
+from ptedit import piece
+
+
+def test_primary():
+    p = piece.PrimaryPiece(data='foo')
+    assert p.data == 'foo'
+    p.extend('bar')
+    assert p.data == 'foobar'
+    assert len(p) == 6
+
+
+def test_secondary():
+    foobar = piece.PrimaryPiece(data='foobar')
+
+    p = piece.SecondaryPiece(prev=foobar, source=foobar, start=2, length=3)
+    assert p.prev == foobar
+    assert p.data == 'oba'
+    p.trim(1,1)
+    assert p.data == 'b'
